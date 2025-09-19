ceae41d2c116dcce9dfd6c94f79258750b639375
Benylaode <bayubulan659@gmail.com>
2025-09-20 06:18:34 +0800

    ganti database

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://vocare:vocare@localhost:5432/vocare"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres.tsrhbyhpsbwrgmauvzur:Vocare123%21%40%23@aws-1-us-east-2.pooler.supabase.com:6543/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "vocarecare" 
     JWT_SECRET_KEY= "vocarecare_jwt"  
+
+# postgresql://vocare:vocare@localhost:5432/vocare
