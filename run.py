ccc54e2b2662f1e19f4ac597eb9f61c55b0e4ab1
Benylaode <bayubulan659@gmail.com>
2025-09-21 20:15:05 +0800

    ubah sedikit lagi perintah

--- run.py
+++ run.py
@@ -7,6 +7,6 @@ with app.app_context():
     db.create_all() 
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port)
