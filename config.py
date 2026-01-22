614a4eb6c9963ed822bf34d7c57a14dfce8e699d
Benylaode <bayubulan659@gmail.com>
2026-01-22 21:47:45 +0800

    menganti database

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres.tsrhbyhpsbwrgmauvzur:Vocare123%21%40%23@aws-1-us-east-2.pooler.supabase.com:6543/postgres"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:vocare123@localhost:5432/vocare"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "vocarecare" 
-    JWT_SECRET_KEY= "vocarecare_jwt"  
+    SECRET_KEY = "vocarecare"
+    JWT_SECRET_KEY = "vocarecare_jwt"
     MAX_CONTENT_LENGTH = 100 * 1024 * 1024
-
-# postgresql://postgres.tsrhbyhpsbwrgmauvzur:Vocare123%21%40%23@aws-1-us-east-2.pooler.supabase.com:6543/postgres
